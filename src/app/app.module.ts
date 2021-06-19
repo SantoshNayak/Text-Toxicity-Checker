5999d99f18fbf8b39f9f7c2fede9e5476406a82b
Santosh Nayak <santoshnayak1996@gmail.com>
2021-06-19 16:50:00 +0530

    ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextComponent } from './text/text.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { NgxUiLoaderModule } from "ngx-ui-loader";
+import {MatTableModule} from '@angular/material/table';
 
+import {MatInputModule} from '@angular/material/input';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +22,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+    NgxUiLoaderModule,
+    MatTableModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import * as toxicity from '@tensorflow-models/toxicity';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
   styleUrls: ['./text.component.scss'],
 })
 export class TextComponent implements OnInit {
-  constructor() {}
+  constructor(public uiLoader: NgxUiLoaderService) {}
   finalPredictions;
+  textToAnalyze:string = 'you a fucking idiot';
+
+  Measuring ='Measuring';
 
   ngOnInit(): void {
+    // this.uiLoader.start();
+
     console.log('on');
 
+  }
+
+  analyseText(){
+    console.log('on');
+
+    this.uiLoader.start();
     // The minimum prediction confidence.
     const threshold = 0.9;
 
@@ -19,8 +31,10 @@ export class TextComponent implements OnInit {
     const labelsToInclude = ['identity_attack', 'insult', 'threat','sexual_explicit'];
 
     toxicity.load(threshold, labelsToInclude).then((model) => {
+      this.uiLoader.stop();
+
       // Now you can use the `model` object to label sentences.
-      model.classify(['you a fucking idiot']).then((predictions) => {
+      model.classify([this.textToAnalyze]).then((predictions) => {
         console.log(predictions);
         this.finalPredictions = predictions;
 
